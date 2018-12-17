14489ebfd1965ea6df1b6dcb1ed6208dd93bcd5b
ThanhPhu <htp1912@gmail.com>
2018-12-17 14:32:06 +0700

    remove HystrixSSEStream

--- index.js
+++ index.js
@@ -3,5 +3,4 @@ module.exports = {
    metricsFactory: require("./lib/metrics/CommandMetrics").Factory,
    circuitFactory: require("./lib/command/CircuitBreaker"),
    hystrixConfig: require("./lib/util/HystrixConfig"),
-   hystrixSSEStream: require("./lib/http/HystrixSSEStream")
 };
